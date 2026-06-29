0a9e7f92275226648f5ec2f7f8649d86fb242c49
TranDucChinh4326 <22004326@st.vlute.edu.vn>
2026-06-29 14:05:56 +0700

    Configure Google login

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FOODHUB_CONFIG = {
   API_BASE_URL: "https://food-backend-xrb9.onrender.com/api",
-  GOOGLE_CLIENT_ID: "",
+  GOOGLE_CLIENT_ID: "738762709250-ejdg7co0608ddn45bm75jrsnveu36vmv.apps.googleusercontent.com",
   FACEBOOK_APP_ID: ""
 };
